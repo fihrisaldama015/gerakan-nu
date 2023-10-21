ed474b1a4e9b2cbe2a6731f620461dd9d72b74bf
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-10-21 14:01:40 +0700

    [FIX] navbar hidden in desktop

--- components/molecules/Navbar.tsx
+++ components/molecules/Navbar.tsx
@@ -1,25 +1,29 @@
 "use client";
+import { Transition } from "@headlessui/react";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import NavbarDropdown from "../NavbarDropdown";
 import NavbarMenu from "./NavbarMenu";
-import { useState } from "react";
-import { Transition } from "@headlessui/react";
 
 function Navbar({ isLogin }: { isLogin: boolean }) {
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const [windowWidth, setWindowWidth] = useState(0);
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+  }, []);
   return (
     <div className="flex items-center">
       <Transition
-        show={isDropdownOpen}
+        show={windowWidth > 640 ? true : isDropdownOpen}
         enter="transition-all duration-300"
         enterFrom="-translate-x-96"
         enterTo="translate-x-0"
         leave="transition-all duration-300"
         leaveFrom="translate-x-0"
         leaveTo="-translate-x-96"
-        className="absolute left-0 top-0 h-screen sm:relative bg-gradient-to-b from-blue_primary to-white sm:bg-transparent sm:flex flex flex-col gap-4 items-center"
+        className="absolute left-0 top-0 h-screen sm:h-fit sm:relative bg-gradient-to-b from-blue_primary to-white sm:from-white sm:flex flex flex-col gap-4 items-center"
       >
-        <div className="p-2.5 flex justify-center items-center">
+        <div className="p-2.5 sm:hidden flex justify-center items-center">
           <Image
             src="/logo.svg"
             alt="logo"
@@ -29,7 +33,7 @@ function Navbar({ isLogin }: { isLogin: boolean }) {
           />
         </div>
         <div className="sm:hidden w-full border border-b-1 border-white/50"></div>
-        <div className="p-6 sm:text-slate-900 text-white flex flex-1 flex-col justify-between">
+        <div className="p-6 sm:text-slate-900 text-white flex flex-1 flex-col sm:flex-row sm:items-center justify-between">
           <NavbarDropdown isLogin={isLogin} />
           <NavbarMenu isLogin={isLogin} />
         </div>
